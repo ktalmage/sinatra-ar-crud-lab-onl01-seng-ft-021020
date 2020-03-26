37cd6e56259a027555b0e13c4a5d51871972fbb2
Kyle Talmage <ktalmage85@gmail.com>
2020-03-26 19:30:55 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,13 +41,13 @@ class ApplicationController < Sinatra::Base
       new_params[:title] = params["title"]
       new_params[:content] = params["content"]
       old_article.update(new_params)
-      
+      binding.pry
       redirect "/articles/#{id}"
    
   end
   
   delete '/articles/:id/delete' do
-    binding.pry
+  
     @article = Article.find(params["id"])
     @article.destroy
     erb :delete
