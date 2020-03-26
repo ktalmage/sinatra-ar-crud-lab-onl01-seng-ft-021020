edb69478a2dbbcafc44fcf6d4d120236eeac3215
Kyle Talmage <ktalmage85@gmail.com>
2020-03-26 19:24:26 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,50 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+ 
+ get '/articles' do
+     @articles = Article.all
+     erb :index
+   end
+ 
+ # new page
+  get '/articles/new' do
+    erb :new
+  end
+  
+  #takes in user input and creates an article
+  post '/articles' do
+    article = Article.create(params)
+    redirect "/articles/#{article.id}"
+  end
+  
+  #show page - should show data specific to one article
+    get '/articles/:id' do
+      @article = Article.find_by(id:params[:id])
+      erb :show
+    end
+  
+    get '/articles/:id/edit' do
+      @article = Article.find(params[:id])
+      erb :edit
+    end
+    
+    patch '/articles/:id' do
+      binding.pry
+      new_params = {}
+      old_article = Article.find(params["id"])
+      new_params[:title] = params["title"]
+      new_params[:content] = params["content"]
+      old_article.update(new_params)
 
-  get '/' do
+      redirect "/articles/#{id}"
+   
+  end
+  
+  delete '/articles/:id/delete' do
+    @article = Article.find(params["id"])
+    @article.destroy
+    erb :delete
+
   end
 end
