57a4a2d79efc1fd57ddc865b37fde75d2aea0ad9
Kyle Talmage <ktalmage85@gmail.com>
2020-03-26 19:32:55 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,7 +41,6 @@ class ApplicationController < Sinatra::Base
       new_params[:title] = params["title"]
       new_params[:content] = params["content"]
       old_article.update(new_params)
-      binding.pry
       redirect "/articles/#{id}"
    
   end
